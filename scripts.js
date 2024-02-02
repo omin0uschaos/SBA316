137a0f8bbbff7bcc1e44f4b58ac4f00dae73306d
Joseph Aleman <isjoecool@yahoo.com>
2024-02-01 21:14:46 -0800

    updated webpage, added h2
    setting up page structures.

--- scripts.js
+++ scripts.js
@@ -1,5 +1,18 @@
 class Person {
-    constructor(firstname, lastname, nickname, age, homeplanet, militaryfaction, rank, rankclass, jobtitle, dateofbirth, shortbio, longbio){
-
+    constructor(firstname, lastname, nickname, age, homeplanet, militaryfaction, rank, rankclass, jobtitle, dateofbirth, shortbio, longbio, skills){
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.nickname = nickname;
+        this.age = age;
+        this.homeplanet = homeplanet;
+        this.militaryfaction = militaryfaction;
+        this.rank = rank;
+        this.rankclass = rankclass;
+        this.jobtitle = jobtitle;
+        this.dateofbirth = dateofbirth;
+        this.shortbio = shortbio;
+        this.longbio = longbio;
+        this.skills = skills;
     }
 }
+
